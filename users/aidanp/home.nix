a85576ead09ee19332c19038e42ac31b0005e7ed
Aidan Pinard <aidan@aidanpinard.co>
2022-10-18 10:42:14 -0400

    Initial commit w/ NixOS config
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>

---
+++ users/aidanp/home.nix
@@ -0,0 +1,351 @@
+{ config, pkgs, ... }:
+
+let
+  
+  unstablePkgs = import <unstable> {};
+  
+in {
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "aidanp";
+  home.homeDirectory = "/home/aidanp";
+  
+  # Packages to install for my user
+  home.packages = with pkgs; [
+    unstablePkgs.wezterm
+    thefuck
+    appimage-run
+    exa
+    axel
+    bitwarden
+    git-crypt
+    obsidian
+    obs-studio
+    wl-clipboard
+  ];
+
+  # Programs with configuration in home manager
+  programs = {
+    fish = {
+      enable = true;
+      plugins = with pkgs; [ 
+        {
+          name = "done";
+          src = fishPlugins.done.src;
+        }
+        {
+          name = "tide";
+          src = pkgs.fetchFromGitHub {
+            owner = "IlanCosman";
+            repo = "tide";
+            rev = "6833806ba2eaa1a2d72a5015f59c284f06c1d2db";
+            sha256 = "vi4sYoI366FkIonXDlf/eE2Pyjq7E/kOKBrQS+LtE+M=";
+          };
+        }
+        {
+          name = "bang-bang";
+          src = pkgs.fetchFromGitHub {
+            owner = "oh-my-fish";
+            repo = "plugin-bang-bang";
+            rev = "f969c618301163273d0a03d002614d9a81952c1e";
+            sha256 = "1r3d4wgdylnc857j08lbdscqbm9lxbm1wqzbkqz1jf8bgq2rvk03";
+          };
+        }
+        {
+          name = "thefuck";
+          src = pkgs.fetchFromGitHub {
+            owner = "oh-my-fish";
+            repo = "plugin-thefuck";
+            rev = "6c9a926d045dc404a11854a645917b368f78fc4d";
+            sha256 = "1n6ibqcgsq1p8lblj334ym2qpdxwiyaahyybvpz93c8c9g4f9ipl";
+          };
+        }
+      ];
+    };
+    fzf = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+    bat.enable = true;
+    helix = {
+      enable = true;
+      package = unstablePkgs.helix;
+      languages = [
+        {
+          name = "rust";
+          formatter = { 
+            command = "rustfmt"; 
+            args = [ "--edition" "2021" ];
+          };
+        }
+        {
+          name = "c";
+          indent = {
+            tab-width = 4;
+            unit = " ";
+          };
+        }
+      ];
+      settings = {
+        theme = "gruvbox_original_dark_hard";
+        editor = {
+          shell = ["fish" "-c"];
+          idle-timeout = 400;
+          rulers = [120];
+          color-modes = true;
+          cursorline = true;
+          statusline = {
+            left = [ "mode" "spinner" "diagnostics" "file-name" ];
+            center = [ "file-type" ];
+            right = [ "selections" "file-encoding" "position-percentage" "position" ];
+            separator = "|";
+          };
+          lsp = {
+            display-messages = true;
+          };
+          cursor-shape = {
+            insert = "bar";
+            select = "underline";
+          };
+          file-picker = {
+            max-depth = 5;
+          };
+          auto-pairs = {
+            "(" = ")";
+            "{" = "}";
+            "[" = "]";
+            "\"" = "\"";
+            "`" = "`";
+            "<" = ">";
+          };
+          indent-guides = {
+            render = true;
+          };
+        };
+      };
+      themes = {
+        gruvbox_original_dark_hard = let 
+            bg0 = "#1d2021";
+            bg1 = "#282828";
+            bg2 = "#282828";
+            bg3 = "#3c3836";
+            bg4 = "#3c3836";
+            bg5 = "#504945";
+            bg_statusline1 = "#282828";
+            bg_statusline2 = "#32302f";
+            bg_statusline3 = "#504945";
+            bg_diff_green = "#32361a";
+            bg_visual_green = "#333e34";
+            bg_diff_red = "#3c1f1e";
+            bg_visual_red = "#442e2d";
+            bg_diff_blue = "#0d3138";
+            bg_visual_blue = "#2e3b3b";
+            bg_visual_yellow = "#473c29";
+            bg_current_word = "#32302f";
+            fg0 = "#ebdbb2";
+            fg1 = "#ebdbb2";
+            red = "#fb4934";
+            orange = "#fe8019";
+            yellow = "#fabd2f";
+            green = "#b8bb26";
+            aqua = "#8ec07c";
+            blue = "#83a598";
+            purple = "#d3869b";
+            bg_red = "#cc241d";
+            bg_green = "#b8bb26";
+            bg_yellow = "#fabd2f";
+            grey0 = "#7c6f64";
+            grey1 = "#928374";
+            grey2 = "#a89984";
+          in
+            {
+              "type" = yellow;
+              "constant" = purple;
+              "constant.numeric" = purple;
+              "constant.character.escape" = orange;
+              "string" = green;
+              "string.regexp" = blue;
+              "comment" = grey0;
+              "variable" = fg0;
+              "variable.builtin" = blue;
+              "variable.parameter" = fg0;
+              "variable.other.member" = fg0;
+              "label" = aqua;
+              "punctuation" = grey2;
+              "punctuation.delimiter" = grey2;
+              "punctuation.bracket" = fg0;
+              "keyword" = red;
+              "keyword.directive" = aqua;
+              "operator" = orange;
+              "function" = green;
+              "function.builtin" = blue;
+              "function.macro" = aqua;
+              "tag" = yellow;
+              "namespace" = aqua;
+              "attribute" = aqua;
+              "constructor" = yellow;
+              "module" = blue;
+              "special" = orange;
+              "markup.heading.marker" = grey2;
+              "markup.heading.1" = { fg = red; modifiers = ["bold"]; };
+              "markup.heading.2" = { fg = orange; modifiers = ["bold"]; };
+              "markup.heading.3" = { fg = yellow; modifiers = ["bold"]; };
+              "markup.heading.4" = { fg = green; modifiers = ["bold"]; };
+              "markup.heading.5" = { fg = blue; modifiers = ["bold"]; };
+              "markup.heading.6" = { fg = "fg"; modifiers = ["bold"]; };
+              "markup.list" = red;
+              "markup.bold" = { modifiers = ["bold"]; };
+              "markup.italic" = { modifiers = ["italic"]; };
+              "markup.link.url" = { fg = blue; modifiers = ["underlined"]; };
+              "markup.link.text" = purple;
+              "markup.quote" = grey2;
+              "markup.raw" = green;
+              "diff.plus" = green;
+              "diff.delta" = orange;
+              "diff.minus" = red;
+              "ui.background" = { bg = bg0; };
+              "ui.background.separator" = grey0;
+              "ui.cursor" = { fg = bg0; bg = fg0; };
+              "ui.cursor.match" = { fg = orange; bg = bg_visual_yellow; };
+              "ui.cursor.insert" = { fg = bg0; bg = grey2; };
+              "ui.cursor.select" = { fg = bg0; bg = blue; };
+              "ui.cursorline.primary" = { bg = bg1; };
+              "ui.cursorline.secondary" = { bg = bg1; };
+              "ui.selection" = { bg = bg3; };
+              "ui.linenr" = grey0;
+              "ui.linenr.selected" = fg0;
+              "ui.statusline" = { fg = fg0; bg = bg3; };
+              "ui.statusline.inactive" = { fg = grey0; bg = bg1; };
+              "ui.statusline.normal" = { fg = bg0; bg = fg0; modifiers = ["bold"]; };
+              "ui.statusline.insert" = { fg = bg0; bg = yellow; modifiers = ["bold"]; };
+              "ui.statusline.select" = { fg = bg0; bg = blue; modifiers = ["bold"]; };
+              "ui.bufferline" = { fg = grey0; bg = bg1; };
+              "ui.bufferline.active" = { fg = fg0; bg = bg3; modifiers = ["bold"]; };
+              "ui.popup" = { fg = grey2; bg = bg2; };
+              "ui.window" = { fg = grey0; bg = bg0; };
+              "ui.help" = { fg = fg0; bg = bg2; };
+              "ui.text" = fg0;
+              "ui.text.focus" = fg0;
+              "ui.menu" = { fg = fg0; bg = bg3; };
+              "ui.menu.selected" = { fg = bg0; bg = blue; modifiers = ["bold"];  };
+              "ui.virtual.whitespace" = { fg = bg4; };
+              "ui.virtual.indent-guide" = { fg = bg4; };
+              "ui.virtual.ruler" = { bg = bg3; };
+              "hint" = blue;
+              "info" = aqua;
+              "warning" = yellow;
+              "error" = red;
+              "diagnostic" = { modifiers = ["underlined"]; };
+            };
+      };
+    };
+    firefox = {
+      enable = true;
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        bitwarden
+        canvasblocker
+        cookies-txt
+        darkreader
+        violentmonkey
+        wayback-machine
+        ublock-origin
+        export-tabs-urls-and-titles
+        stylus
+      ];
+      profiles = {
+        main = {
+          bookmarks = {
+            "Baka-Tsuki" = {
+              url = "https://www.baka-tsuki.org/project/index.php?title=Category:Light_novel_(English)";
+            };
+            "Amazon.com" = {
+              url = "https://www.amazon.com/";
+            };
+            "YouTube" = {
+              url = "https://www.youtube.com/";
+            };
+            "AnimeBytes" = {
+              url = "https://animebytes.tv/torrents.php";
+            };
+            "Google Translate" = {
+              url = "https://translate.google.com/";
+            };
+            "Nyaa.si" = {
+              url = "https://nyaa.si/";
+            };
+            "HDQWalls Anime 1920x1080 Wallpapers" = {
+              url = "http://hdqwalls.com/category/anime-wallpapers/1920x1080";
+            };
+            "[pixiv]" = {
+              url = "https://www.pixiv.net/";
+            };
+            "regex101" = {
+              url = "https://regex101.com/";
+            };
+            "`printf` cheat sheet" = {
+              url = "https://alvinalexander.com/programming/printf-format-cheat-sheet/";
+            };
+            "OneDrive" = {
+              url = "https://onedrive.live.com/";
+            };
+            "Wuxiaworld – Chinese fantasy novels and light novels!" = {
+              url = "http://www.wuxiaworld.com/";
+            };
+            "Just don't. Unless it's a gift for someone you hate." = {
+              url = "https://www.amazon.com/gp/customer-reviews/R3FTHSH0UNRHOH/ref=cm_cr_arp_d_viewpnt?ie=UTF8&ASIN=B00DE4GWWY#R3FTHSH0UNRHOH";
+            };
+            "SauceNAO Image Search" = {
+              url = "https://saucenao.com/index.php";
+            };
+            "Calculator - Jet Box" = {
+              url = "https://jetboxinternational.com/calculator/";
+            };
+            "Welcome To GATE eService" = {
+              url = "http://www.e-gate.gov.tt/gate-app/";
+            };
+            "e-Courier.ca" = {
+              url = "https://e-courier.ca/aQ?is=Zjkl33oH3Y8e&ue=aidan.pinard@my.uwi.edu";
+            };
+          };
+          id = 0;
+        };
+      };
+    };
+    git = {
+      enable = true;
+      delta.enable = true;
+      userName = "Aidan Pinard";
+      userEmail = "aidan@aidanpinard.co";
+      signing = {
+        key = "C48B088F4FFBBDF0";
+        signByDefault = true;
+      };
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
+    };
+    gpg.enable = true;
+  };
+  
+  # Services
+  services.gpg-agent = {
+    enable = true;
+    pinentryFlavor = "gnome3";
+    enableFishIntegration = true;
+    enableSshSupport = true;
+    defaultCacheTtl = 60;
+  };
+  
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
