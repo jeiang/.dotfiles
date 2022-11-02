2bdc52f16972302baef9d6223337461b32e1ba4d
Aidan Pinard <aidan@aidanpinard.co>
2022-11-01 20:49:11 -0400

    Moved persistence to its own nix file
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>

--- system/asus-nixos/configuration.nix
+++ system/asus-nixos/configuration.nix
@@ -9,6 +9,8 @@
     ./filesystems.nix
     # Modified asus battery modules from nixos-hardware. Battery is BAT1 not BAT0 on ASUS TUF FA506IV
     ./hardware/battery.nix
+    # Persistence
+    ./persist.nix
   ];
 
   # Config for flakes from https://github.com/Misterio77/nix-starter-configs
@@ -175,23 +177,6 @@
     fuse.userAllowOther = true;
   };
 
-  # Setting up /etc & /var bindings for persistence
-  environment.etc = {
-    nixos.source = "/persist/etc/nixos";
-    "NetworkManager/system-connections".source =
-      "/persist/etc/NetworkManager/system-connections";
-    adjtime.source = "/persist/etc/adjtime";
-    NIXOS.source = "/persist/etc/NIXOS";
-    machine-id.source = "/persist/etc/machine-id";
-    shadow.source = "/persist/etc/shadow";
-  };
-  systemd.tmpfiles.rules = [
-    "L /var/lib/NetworkManager/secret_key - - - - /persist/var/lib/NetworkManager/secret_key"
-    "L /var/lib/NetworkManager/seen-bssids - - - - /persist/var/lib/NetworkManager/seen-bssids"
-    "L /var/lib/NetworkManager/timestamps - - - - /persist/var/lib/NetworkManager/timestamps"
-    "L /var/lib/lxd - - - - /persist/var/lib/lxd"
-    "L /var/lib/docker - - - - /persist/var/lib/docker"
-  ];
   # Disable sudo lecture every boot because of rollback to blank
   security.sudo.extraConfig = ''
     Defaults lecture = never
