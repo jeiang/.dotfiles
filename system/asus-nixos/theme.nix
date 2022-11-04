d0199a6b87046c1b1183f6d24b4224c95cd9ea5c
Aidan Pinard <aidan@aidanpinard.co>
2022-11-04 08:52:25 -0400

    Fixed minor bug with wallpaper & removed helix theming (to avoid clash)
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>

--- system/asus-nixos/theme.nix
+++ system/asus-nixos/theme.nix
@@ -2,8 +2,12 @@
 
 { inputs, lib, config, pkgs, theme, ... }: {
   stylix = {
-    image = .../../themes/${theme}/wallpaper.png;
+    image = ../../themes/${theme}/wallpaper.png;
     polarity = "dark";
     base16Scheme = "${inputs.base16-schemes}/${theme}.yaml";
+
+    # Disable helix because generated theme is less readable
+    # than custom gruvbox
+    targets.helix = false;
   };
 }
