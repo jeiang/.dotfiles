363bf139b67c07c2d216724b291eab521e99dd78
Aidan Pinard <aidan@aidanpinard.co>
2022-11-04 11:55:42 -0400

    Removed some unwanted gnome apps
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>

---
+++ system/asus-nixos/windowing.nix
@@ -0,0 +1,34 @@
+# Settings for the window manager + desktop environment.
+
+{ inputs, lib, config, pkgs, ... }: {
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    cheese # webcam tool
+    gnome-music
+    gnome-terminal
+    gnome-contacts
+    epiphany # web browser
+    geary # email reader
+    gnome-characters
+    totem # video player
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+  ]);
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+}
