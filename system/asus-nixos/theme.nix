c9507006ec01477c8599680c2c64688f6fb7d245
Aidan Pinard <aidan@aidanpinard.co>
2022-11-04 10:34:55 -0400

    Changed fonts
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>

--- system/asus-nixos/theme.nix
+++ system/asus-nixos/theme.nix
@@ -5,7 +5,19 @@
     image = ../../themes/${theme}/wallpaper.png;
     polarity = "dark";
     base16Scheme = "${inputs.base16-schemes}/${theme}.yaml";
-
+    fonts = rec {
+      serif = sansSerif;
+      sansSerif = {
+        name = "Overpass";
+        package = pkgs.overpass;
+      };
+      monospace = {
+        name = "JetBrainsMono Nerd Font Mono";
+        package = pkgs.nerdfonts.override {
+          fonts = [ "JetBrainsMono" ];
+        };
+      };
+    };
     # Disable helix because generated theme is less readable
     # than custom gruvbox
     targets.helix.enable = false;
