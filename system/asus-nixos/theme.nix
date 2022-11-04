e31c70a5aec69dc0fb839d43c212cd5474c08503
Aidan Pinard <31970261+jeiang@users.noreply.github.com>
2022-11-04 10:06:55 -0400

    Theming (#4)
    * Removed old module for theming.
    
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>
    
    * Created basic theming setup.
    
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>
    
    * Fixed minor bug with wallpaper & removed helix theming (to avoid clash)
    
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>
    
    * Forked Stylix to make gnome use dark mode, also removed some unused stuff
    
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>
    
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>

---
+++ system/asus-nixos/theme.nix
@@ -0,0 +1,13 @@
+# Theming using stylix & base16-schemes
+
+{ inputs, lib, config, pkgs, theme, ... }: {
+  stylix = {
+    image = ../../themes/${theme}/wallpaper.png;
+    polarity = "dark";
+    base16Scheme = "${inputs.base16-schemes}/${theme}.yaml";
+
+    # Disable helix because generated theme is less readable
+    # than custom gruvbox
+    targets.helix.enable = false;
+  };
+}
