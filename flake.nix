6e2cd6b02d5b662c4c13efe683f53658526e0ff4
Aidan Pinard <aidan@aidanpinard.co>
2022-11-08 17:13:54 -0400

    Deleted Original Setup and added Mysterio77's raw standard config
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>

--- flake.nix
+++ flake.nix
@@ -9,79 +9,67 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # NUR
-    nur.url = "github:nix-community/NUR";
+    # TODO: Add any other flake you might need
+    hardware.url = "github:nixos/nixos-hardware";
 
-    # Theming
-    stylix.url = "github:danth/stylix";
-    stylix.inputs = {
-      home-manager.follows = "home-manager";
-      nixpkgs.follows = "nixpkgs";
-    };
-    base16-schemes.url = "github:tinted-theming/base16-schemes";
-    base16-schemes.flake = false;
-
-    # Impermanence
-    impermanence.url = "github:nix-community/impermanence";
-
-    # Nix Formatter
-    nixpkgs-fmt.url = "github:nix-community/nixpkgs-fmt";
-    nixpkgs-fmt.inputs.nixpkgs.follows = "nixpkgs";
+    # Shameless plug: looking for a way to nixify your themes and make
+    # everything match nicely? Try nix-colors!
+    # nix-colors.url = "github:misterio77/nix-colors";
   };
 
-  outputs =
-    { nixpkgs, base16-schemes, home-manager, impermanence, nixpkgs-fmt, nur, stylix, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
-
-      theme = "gruvbox-dark-hard";
-
+      inherit (self) outputs;
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
     in
     rec {
-      formatter = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" ]
-        (system: nixpkgs-fmt.defaultPackage.${system});
+      # Your custom packages
+      # Acessible through 'nix build', 'nix shell', etc
+      packages = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in import ./pkgs { inherit pkgs; }
+      );
+      # Devshell for bootstrapping
+      # Acessible through 'nix develop' or 'nix-shell' (legacy)
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in import ./shell.nix { inherit pkgs; }
+      );
 
-      # This instantiates nixpkgs for each system listed
-      # Allowing you to configure it (e.g. allowUnfree)
-      # Our configurations will use these instances
-      legacyPackages = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" ]
-        (system:
-          import inputs.nixpkgs {
-            inherit system;
-
-            # NOTE: Using `nixpkgs.config` in your NixOS config won't work
-            # Instead, you should set nixpkgs configs here
-            # (https://nixos.org/manual/nixpkgs/stable/#idm140737322551056)
-            config.allowUnfree = true;
-
-            overlays = [
-              # Add nur to pkgs
-              nur.overlay
-            ];
-          });
+      # Your custom packages and modifications, exported as overlays
+      overlays = import ./overlays;
+      # Reusable nixos modules you might want to export
+      # These are usually stuff you would upstream into nixpkgs
+      nixosModules = import ./modules/nixos;
+      # Reusable home-manager modules you might want to export
+      # These are usually stuff you would upstream into home-manager
+      homeManagerModules = import ./modules/home-manager;
 
       nixosConfigurations = {
-        asus-nixos = nixpkgs.lib.nixosSystem rec {
-          pkgs = legacyPackages.x86_64-linux;
-          specialArgs = {
-            inherit inputs;
-            inherit theme;
-          };
+        # FIXME replace with your hostname
+        your-hostname = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
           modules = [
-            home-manager.nixosModules.home-manager
-            impermanence.nixosModules.impermanence
-            nur.nixosModules.nur
-            stylix.nixosModules.stylix
-            (import ./system/asus-nixos/configuration.nix)
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.aidanp = {
-                imports = [
-                  impermanence.nixosModules.home-manager.impermanence
-                  ./users/aidanp/home.nix
-                ];
-              };
-            }
+            # > Our main nixos configuration file <
+            ./nixos/configuration.nix
+          ];
+        };
+      };
+
+      homeConfigurations = {
+        # FIXME replace with your username@hostname
+        "your-username@your-hostname" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            # > Our main home-manager configuration file <
+            ./home-manager/home.nix
           ];
         };
       };
