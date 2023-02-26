5f6886a38158e7658a5d00118918b32ac6e12ef4
Aidan Pinard <aidan@aidanpinard.co>
2023-02-26 01:00:10 -0400

    Removed some unused programs & ssh stuff
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>

--- home-manager/programs/ssh.nix
+++ home-manager/programs/ssh.nix
@@ -2,12 +2,5 @@
   programs.ssh = {
     enable = true;
     compression = true;
-    matchBlocks = {
-      "ecng3006vm" = {
-        hostname = "134.209.75.252";
-        user = "aidanpinard";
-        identityFile = "/home/aidanp/.ssh/id_ed25519";
-      };
-    };
   };
 }
