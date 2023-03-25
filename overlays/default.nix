cc170dd305621b4abc9c2b62382f4df75cadaeca
Aidan Pinard <aidan@aidanpinard.co>
2023-03-25 00:48:35 -0400

    Switched to wezterm from alacritty

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,6 +7,27 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
+    wezterm = prev.wezterm.overrideAttrs (oldAttrs: rec {
+      pname = "wezterm";
+      version = "20230320-124340-559cb7b0";
+
+      src = final.fetchFromGitHub {
+        owner = "wez";
+        repo = pname;
+        rev = version;
+        fetchSubmodules = true;
+        sha256 = "sha256-u9lOK4DV9NM3CUYjMTovCY4XF5Xxg4V+rQwIjioqTec=";
+      };
+
+      # Patch is for an older version of wezterm
+      patches = [];
+
+      cargoDeps = oldAttrs.cargoDeps.overrideAttrs (final.lib.const {
+        name = "${pname}-${version}-vendor.tar.gz";
+        inherit src;
+        outputHash = "sha256-hdGInc6Qx+d1RUUMZF4AEqmZjXok1TIo8wAudfqft7Y=";
+      });
+    });
     # rm-improved = prev.rm-improved.overrideAttrs (oldAttrs: rec {
     #   pname = "rm-improved";
     #   version = "0.14.0";
@@ -14,7 +35,7 @@
     #     owner = "jeiang";
     #     repo = "rip";
     #     rev = "0.14.0";
-    #     sha256 = ""; # FILL THIS IN AFTER FAILED BUILD
+    #     sha256 = final.lib.fakeSha256; # FILL THIS IN AFTER FAILED BUILD
     #   };
 
     #   nativeBuildInputs = [ final.installShellFiles ];
@@ -32,7 +53,7 @@
     #   cargoDeps = oldAttrs.cargoDeps.overrideAttrs (final.lib.const {
     #     name = "${pname}-${version}-vendor.tar.gz";
     #     inherit src;
-    #     outputHash = ""; # FILL THIS IN AFTER FAILED BUILD
+    #     outputHash = final.lib.fakeSha256; # FILL THIS IN AFTER FAILED BUILD
     #   });
     # });
   };
