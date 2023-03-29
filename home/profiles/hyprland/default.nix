b33e6217cd786311fa990ba541a1c9474d612fbb
Aidan Pinard <aidan@aidanpinard.co>
2023-03-29 02:43:15 -0400

    Added screenshot using grim + slurp (and screensharing??)

--- home/profiles/hyprland/default.nix
+++ home/profiles/hyprland/default.nix
@@ -1,10 +1,19 @@
 { pkgs, lib, config, ... }:
 let
+  screenshot-script = pkgs.writeScriptBin "screenshot" ''
+    # xdg wack so...
+    # normally grim $(xdg-user-dir PICTURES)/$(date +'%s_grim.png')
+    mkdir -p ~/Pictures
+    file=~/Pictures/$(date +'screenshot_%Y-%d-%m-%T.png')
+    ${pkgs.grim}/bin/grim -g "$(${pkgs.slurp}/bin/slurp)" "$file"
+    cat $file | ${pkgs.wl-clipboard}/bin/wl-copy
+  '';
   hypr-conf-keys = {
     "{{mako}}" = "${pkgs.mako}/bin/mako";
     "{{hyprpaper}}" = "${pkgs.hyprpaper}/bin/hyprpaper";
     "{{wezterm}}" = "${pkgs.wezterm}/bin/wezterm";
     "{{firefox}}" = "${pkgs.firefox}/bin/firefox";
+    "{{screenshot}}" = "${screenshot-script}/bin/screenshot";
   };
   hyprpaper-conf-keys = {
     "{{wallpaper}}" = "${config.stylix.image}";
@@ -20,6 +29,8 @@ in
   home.packages = with pkgs; [
     wl-clipboard
     hyprpaper
+    grim
+    slurp
   ];
 
   xdg.configFile."hypr/hyprpaper.conf" = {
