03618d8f2aacd67dad9cc7079b94beddc569f00a
Aidan Pinard <aidan@aidanpinard.co>
2023-11-06 11:06:27 -0400

    feat(impermanence): added more paths to home

--- modules/home/impermanence/default.nix
+++ modules/home/impermanence/default.nix
@@ -15,6 +15,28 @@
       "Templates"
       "Videos"
       ".mozilla"
+      ".renpy"
+      ".local/share/direnv"
+      ".local/share/fish"
+      ".local/share/mcfly"
+      ".local/share/zoxide"
+      ".config/discord"
+      ".cache/bat"
+      ".cache/nix"
+      ".cache/nix-index"
+      {
+        directory = ".local/share/Steam";
+        method = "symlink";
+      }
+    ];
+    files = [
+      ".ssh/id_ed25519"
+      ".ssh/id_ed25519.pub"
+      ".ssh/id_rsa"
+      ".ssh/id_rsa.pub"
+      ".ssh/known_hosts"
+      ".config/fish/fish_variables"
+      ".config/cachix/config.dhall"
     ];
   };
 }
