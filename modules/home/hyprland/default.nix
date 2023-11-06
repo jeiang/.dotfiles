66dfba572db359f9ac09c8fc941f9062d10ecb1f
Aidan Pinard <aidan@aidanpinard.co>
2023-11-06 15:33:22 -0400

    feat(hyprland): switch back to wezterm

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -1,4 +1,5 @@
-{pkgs, ...}: let
+{ pkgs, ... }:
+let
   hyprconf = ''
     source = ${./hyprland.conf}
 
@@ -10,9 +11,9 @@
     $mainMod = SUPER
 
     # Launch Apps
-    bind = $mainMod, return, exec, ${pkgs.alacritty}/bin/alacritty
+    # bind = $mainMod, return, exec, ${pkgs.alacritty}/bin/alacritty
     # See https://github.com/wez/wezterm/issues/4483
-    # bind = $mainMod, return, exec, ${pkgs.wezterm}/bin/wezterm
+    bind = $mainMod, return, exec, ${pkgs.wezterm}/bin/wezterm
     bind = $mainMod, W, exec, ${pkgs.firefox}/bin/firefox
     # bind = $mainMod, R, exec, tofi-drun | xargs hyprctl dispatch exec --
     # bind = $mainMod SHIFT, R, exec, tofi-run | xargs hyprctl dispatch exec --
@@ -23,7 +24,7 @@
 
     # Shortcuts
     ## Screenshot
-    bind = $mainMod SHIFT, S, exec, grimblast --notify copysave area
+    bind = $mainMod SHIFT, S, exec, ${pkgs.grimblast}/bin/grimblast --notify copysave area
 
     # Audio using wireplumber
     binde=, XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+
@@ -34,7 +35,8 @@
     # bindle=, XF86MonBrightnessDown, exec, brightnessctl set 5%-
 
   '';
-in {
+in
+{
   wayland.windowManager.hyprland = {
     enable = true;
     package = null; # Use nixos version
