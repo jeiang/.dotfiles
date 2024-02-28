d436dc4541e0f7fb3ad8c4523c48caccb219b5b2
Aidan Pinard <aidan@aidanpinard.co>
2024-02-28 19:14:29 -0400

    chore: remove old caches

--- modules/system/nix/default.nix
+++ modules/system/nix/default.nix
@@ -34,22 +34,16 @@
       keep-outputs = true;
 
       substituters = [
-        "https://cache.privatevoid.net"
         "https://helix.cachix.org"
         "https://hyprland.cachix.org"
         "https://jeiang.cachix.org"
         "https://nix-community.cachix.org"
-        "https://nix-gaming.cachix.org"
-        "https://nrdxp.cachix.org"
       ];
       trusted-public-keys = [
-        "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "jeiang.cachix.org-1:Ax2onCzp6V74ORnjlTAbZsDmlLeMMzDOzzcC2qHfJKg="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        "nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4="
       ];
       trusted-users = [ "root" "@wheel" ];
     };
