e3a1765f08b95238a8edf69e374c1b7b7051b524
Aidan Pinard <aidan@aidanpinard.co>
2024-03-08 15:29:40 -0400

    fix: disable cd alias in fish due to infinite recursion error

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -10,7 +10,7 @@
     shellAliases = {
       cat = pkgs.lib.mkIf config.programs.bat.enable "${pkgs.bat}/bin/bat";
       # Only use cd -> z if zoxide has generated the binding
-      cd = pkgs.lib.mkIf (config.programs.zoxide.enable && config.programs.zoxide.enableFishIntegration) "z"; # autojump
+      # cd = pkgs.lib.mkIf (config.programs.zoxide.enable && config.programs.zoxide.enableFishIntegration) "z"; # autojump
     };
     shellAbbrs = {
       # home manager doesn't support setting cursor position
