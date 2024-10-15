45cf53664930d66d2f785b2b32a9582fcd0523c8
Aidan Pinard <aidan@aidanpinard.co>
2024-10-14 23:15:30 -0400

    chore: remove unused shell features

--- devenv.nix
+++ devenv.nix
@@ -5,10 +5,6 @@ _: {
       programs = {
         nixpkgs-fmt.enable = true;
         deadnix.enable = true;
-        prettier.enable = true;
-        shellcheck.enable = true;
-        stylua.enable = true;
-        taplo.enable = true;
       };
     };
     formatter = config.treefmt.build.wrapper;
@@ -17,22 +13,15 @@ _: {
         name = "sysconf-dev";
         packages = with pkgs; [
           config.treefmt.build.wrapper
-          eza
           git
           helix
           nixVersions.latest
-          ripgrep
           just
           sops
-          age
-          ssh-to-age
-          nixos-rebuild
-          editorconfig-checker
           nh
         ];
         env.FLAKE = ./.;
         languages = {
-          lua.enable = true;
           nix.enable = true;
         };
         pre-commit.hooks = {
