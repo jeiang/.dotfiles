b65011ec8e92209650250d92c4ecfc882c76aad5
Aidan Pinard <aidan@aidanpinard.co>
2025-10-08 14:50:25 -0400

    chore: add extra substituters

--- modules/nix.nix
+++ modules/nix.nix
@@ -39,11 +39,13 @@
         "https://hyprland.cachix.org"
         "https://jeiang.cachix.org"
         "https://nix-community.cachix.org"
+        "https://walker.cachix.org"
       ];
       trusted-public-keys = [
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "jeiang.cachix.org-1:Ax2onCzp6V74ORnjlTAbZsDmlLeMMzDOzzcC2qHfJKg="
+        "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       trusted-users = [
