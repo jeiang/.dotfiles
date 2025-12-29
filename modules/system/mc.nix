60949177163d0a06449ea1a12dc544b8825b63b9
Aidan Pinard <aidan@aidanpinard.co>
2025-12-28 22:56:21 -0400

    chore: add more mc mods

--- modules/system/mc.nix
+++ modules/system/mc.nix
@@ -31,6 +31,18 @@
                 url = "https://cdn.modrinth.com/data/uXXizFIs/versions/CtMpt7Jr/ferritecore-8.0.0-fabric.jar";
                 sha512 = "12pg6yw3dn1x2rg1xwz8bgb20w894jwxn77v8ay5smfpymkbd184fnad34byqb23b9hdh9hry7dc16ncb1kijxz6mj9dw36sg8q46qk";
               };
+              Lithium = pkgs.fetchurl {
+                url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/qxIL7Kb8/lithium-fabric-0.18.1%2Bmc1.21.8.jar";
+                sha512 = "1r4c72dv62cm276bzwjm7q4y8n0l3578hj03690x15r612sh3j8mkkvivmzmljslf5l5r64lc7xnwzzyjhs9bymrd9c6cf8qwh0hgpg";
+              };
+              Dungeons-And-Taverns = pkgs.fetchurl {
+                url = "https://cdn.modrinth.com/data/tpehi7ww/versions/upyHsGeL/dungeons-and-taverns-v4.7.3.jar";
+                sha512 = "3a905micd10c4jxnanyh5qjs08yzkf0zb2cs0xfq686m3xxj3va22qw2xfxbz9pxbzglhmmqzvidm55snf1w9zc3id4m1ng4xvdziw2";
+              };
+              Essential-Commands = pkgs.fetchurl {
+                url = "https://cdn.modrinth.com/data/6VdDUivB/versions/BBQodJEo/essential_commands-0.38.5-mc1.21.8.jar";
+                sha512 = "2fk5xzfh4hap8hzi2c652r4h5ipayd971xc1y4lwviqh11y9nc79dy3j2h07qmph3aakxwm48f00d8xfmgyzmxc1chaicp7mdyzwzzg";
+              };
             }
           );
         };
