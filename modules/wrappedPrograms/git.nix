33d8d9c6fc73d9a357f01a78d5d94f62479f8b56
Aidan Pinard <aidan@aidanpinard.co>
2026-03-26 01:35:48 -0400

    refactor: git

--- modules/wrappedPrograms/git.nix
+++ modules/wrappedPrograms/git.nix
@@ -1,13 +1,69 @@
 {inputs, ...}: {
-  perSystem = {pkgs, ...}: {
-    packages.git = inputs.wrappers.lib.wrapPackage {
+  perSystem = {
+    pkgs,
+    lib,
+    self',
+    ...
+  }: {
+    packages.difft = inputs.wrapper-modules.lib.wrapPackage (_: {
       inherit pkgs;
-      package = pkgs.git;
-      env = rec {
-        GIT_AUTHOR_NAME = "Aidan Pinard";
-        GIT_AUTHOR_EMAIL = "aidan@aidanpinard.co";
-        GIT_COMMITTER_NAME = GIT_AUTHOR_NAME;
-        GIT_COMMITTER_EMAIL = GIT_AUTHOR_EMAIL;
+      package = pkgs.difftastic;
+      flags = {
+        "--display" = "inline";
+      };
+      flagSeparator = "=";
+    });
+    packages.git = inputs.wrapper-modules.wrappers.git.wrap {
+      inherit pkgs;
+      settings = {
+        user = {
+          name = "Aidan Pinard";
+          email = "aidan@aidanpinard.co";
+        };
+        signing = {
+          key = "C48B088F4FFBBDF0";
+          signByDefault = true;
+        };
+        init.defaultBranch = "main";
+        push.autoSetupRemote = "true";
+        pull.rebase = false;
+        commit.gpgsign = true;
+
+        alias = {
+          a = "!git add -p";
+          co = "!git checkout";
+          cob = "!git checkout -b";
+          f = "!git fetch -p";
+          c = "!git commit";
+          p = "!git push";
+          ba = "!git branch -a";
+          bd = "!git branch -d";
+          bD = "!git branch -D";
+          d = "!git diff";
+          dc = "!git diff --cached";
+          ds = "!git diff --staged";
+          r = "!git restore";
+          rs = "!git restore --staged";
+          st = "!git status -sb";
+
+          # reset
+          soft = "!git reset --soft";
+          hard = "!git reset --hard";
+          s1ft = "!git soft HEAD~1";
+          h1rd = "!git hard HEAD~1";
+
+          # logging
+          lg = "!git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+          plog = "!git log --graph --pretty='format:%C(red)%d%C(reset) %C(yellow)%h%C(reset) %ar %C(green)%aN%C(reset) %s'";
+          tlog = "!git log --stat --since='1 Day Ago' --graph --pretty=oneline --abbrev-commit --date=relative";
+          rank = "!git shortlog -sn --no-merges";
+
+          # delete merged branches
+          bdm = "!git !git branch --merged | grep -v '*' | xargs -n 1 git branch -d";
+        };
+        diff.external = "${lib.getExe self'.packages.difft}";
+        diff.tool = lib.mkDefault "difftastic";
+        difftool.difftastic.cmd = "${lib.getExe self'.packages.difft} $LOCAL $REMOTE";
       };
     };
   };
