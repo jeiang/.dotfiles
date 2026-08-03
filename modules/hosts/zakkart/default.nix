48ab188139afd1089b35d8c02ae9bd08c3149782
Aidan Pinard <aidan@aidanpinard.co>
2026-08-03 15:11:57 -0400

    feat(hosts): add zakkart darwin host
    flake.darwinConfigurations.zakkart via inputs.nix-darwin.lib.darwinSystem,
    composing the modules/darwin/* pieces the same way
    modules/hosts/artemis/default.nix composes modules/nixos/*.
    
    Verified: `nix build .#darwinConfigurations.zakkart.config.system.build.toplevel`
    succeeds on this aarch64-darwin machine.

---
+++ modules/hosts/zakkart/default.nix
@@ -0,0 +1,28 @@
+{
+  inputs,
+  self,
+  ...
+}: {
+  flake = {
+    darwinConfigurations.zakkart = inputs.nix-darwin.lib.darwinSystem {
+      modules = [
+        self.darwinModules.zakkartConfiguration
+      ];
+    };
+
+    darwinModules.zakkartConfiguration = _: {
+      imports = [
+        self.darwinModules.base
+        self.darwinModules.nix
+        self.darwinModules.hjem
+        self.darwinModules.sops
+        self.darwinModules.homebrew
+        self.darwinModules.apps
+        self.darwinModules.netbird
+        self.darwinModules.system
+      ];
+
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  };
+}
